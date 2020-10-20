e2e9e6e6677fc6e91ce9e86e563d4ab21182150f
mangjiangyu <1220483971@qq.com>
2020-10-20 08:47:15 +0800

    2048

---
+++ pages/mine/mine.js
@@ -0,0 +1,89 @@
+// pages/mine.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    gonglve:[
+      {
+        imgurl:"/assets/捕获.PNG",
+        title:"玩2048的高分小技巧",
+        url:"https://www.bilibili.com/video/BV1nx411y7ZS?from=search&seid=17722150496495118303"
+      },
+      {
+        imgurl:"/assets/1.PNG",
+        title:"【2048技巧】如何做到6分钟快速合成2048？",
+        url:"https://www.bilibili.com/video/BV1at411g7b5?from=search&seid=17722150496495118303"
+      },
+      {
+        imgurl:"/assets/2.PNG",
+        title:"2048入门攻略",
+        url:"https://www.bilibili.com/video/BV1bs41167bj?from=search&seid=5294426435676967206"
+      },
+      {
+        imgurl:"/assets/3.PNG",
+        title:"手残党的逆袭！2048新手教程",
+        url:"https://www.bilibili.com/video/BV1Tx411T7RG?from=search&seid=5294426435676967206"
+      },
+      
+    ]
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
